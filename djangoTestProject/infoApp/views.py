62933b2e4c056aacd5434e24c5819e752749132c
mehmetteren <eren.balasar@ug.bilkent.edu.tr>
2022-11-17 11:39:18 +0300

    test project added

---
+++ djangoTestProject/infoApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from testApp.models import Student
+# Create your views here.
+
+
+def home(request):
+    return
---
+++ djangoTestProject/testApp/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Student(models.Model):
+    student_id = models.CharField(max_length=10)
+    name = models.CharField(max_length=40)
+    surname = models.CharField(max_length=40)
+    department = models.CharField(max_length=50, default="")
+    email = models.EmailField(max_length=80)
+    image = models.ImageField(upload_to='testApp/images/')
+    url = models.URLField(blank=True)
+
+
+class ApplyingStudent(Student):
+    points = models.FloatField(help_text="Erasmus points of the student")
+
+
+class University(models.Model):
+    name = models.CharField(max_length=80)
+    contactInfo = models.EmailField(max_length=80)
+    rating = models.FloatField(help_text="Rating of the university",
+                               verbose_name="should be out of 5")
+
+
+class FormerStudent(Student):
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
