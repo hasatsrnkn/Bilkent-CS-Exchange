dbe07f27409490765c77e76fb651782cd8d697f9
mehmetteren <eren.balasar@ug.bilkent.edu.tr>
2022-11-19 23:45:27 +0300

    admin interface updated, minor changes to some models

--- src/MigrationApp/models/ActorModels.py
+++ src/MigrationApp/models/ActorModels.py
@@ -6,8 +6,8 @@ class User(models.Model):
     surname = models.CharField(max_length=100, default='')
     email = models.EmailField(max_length=100, default='')
 
-    #class Meta:
-     #   abstract = True
+    class Meta:
+        verbose_name = "User"
 
 
 class Student(User):
@@ -20,15 +20,16 @@ class ExchangeOffice(User):
 
 
 class Management(User):
-    check_list = models.ForeignKey('MigrationApp.ToDoList', on_delete=models.CASCADE)
+    check_list = models.ForeignKey('MigrationApp.ToDoList', blank=True,
+                                   on_delete=models.CASCADE)
 
 
 class ApplyingStudent(Student):
-    check_list = models.OneToOneField('MigrationApp.ToDoList', on_delete=models.CASCADE)
+    check_list = models.OneToOneField('MigrationApp.ToDoList', blank=True,
+                                      on_delete=models.CASCADE)
 
 
 class FormerStudent(Student):
-    checkList = models.ForeignKey('MigrationApp.ToDoList', on_delete=models.CASCADE)
     uni_visited = models.ForeignKey('MigrationApp.University', on_delete=models.CASCADE)
     begin_date = models.DateField(max_length='20', default='')
     end_date = models.DateField(max_length='20', default='')
--- src/MigrationApp/models/SystemModels.py
+++ src/MigrationApp/models/SystemModels.py
@@ -8,11 +8,14 @@ class ToDoList(models.Model):
 
 #not finished
 class Notification(models.Model):
-    text = models.CharField(max_length=100, default='')
+    text = models.CharField(max_length=100, default='', blank=True)
     user = models.ForeignKey('MigrationApp.User', on_delete=models.CASCADE)
 
 
 #not finished
 class University(models.Model):
     name = models.CharField(max_length=100, default='')
+
+    class Meta:
+        verbose_name_plural = 'Universities'
     
