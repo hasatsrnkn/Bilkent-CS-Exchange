8fb84f2f8caaee8155215997a3d99e876c781f81
Hasat Serinkan <hasat.serinkan@ug.bilkent.edu.tr>
2022-12-14 19:32:21 +0300

    api changed and spinner added

--- csexchange/components/mainMenuPage/ContinueAsGuest.js
+++ csexchange/components/mainMenuPage/ContinueAsGuest.js
@@ -1,9 +1,20 @@
 import { Button } from "react-bootstrap";
 import Link from "next/link";
+import { loadingActions } from "../../store/loading";
+import { useDispatch } from "react-redux";
+
+
 const ContinueAsGuest = () => {
+
+  const dispatch = useDispatch();
+
+  const loadingHandler = () => {
+    dispatch(loadingActions.setIsLoading())
+  }
+
   return (
     <Link className="" href="/universities" passHref legacyBehavior>
-      <Button className="mt-5" size="lg" variant="danger">Continue As Guest</Button>
+      <Button className="mt-5" size="lg" variant="danger" onClick={loadingHandler}>Continue As Guest</Button>
     </Link>
   );
 };
